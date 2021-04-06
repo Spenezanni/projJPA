0afceb75d46b323ae78975bea0635a2474825bde
Luiz Gustavo <gustavospenezanni@gmail.com>
2021-04-06 09:35:21 -0300

    init commit

---
+++ src/main/java/br/com/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,35 @@
+package br.com.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.loja.dao.CategoriaDao;
+import br.com.loja.dao.ProdutoDao;
+import br.com.loja.modelo.Categoria;
+import br.com.loja.modelo.Produto;
+import br.com.loja.util.JPAUtil;
+
+public class CadastroDeProduto {
+	
+	public static void main(String[] args) {
+		
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Motorola", "A venda", new BigDecimal("1000"), celulares );
+	
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		
+		em.getTransaction().begin();
+		
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
